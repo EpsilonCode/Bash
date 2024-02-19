74d99e9f3e85c028c158bfb79b350f901f355b07
EpsilonCode <129466561+EpsilonCode@users.noreply.github.com>
2024-02-18 16:15:38 -0900

    Update getGoogleChrome.sh

--- getGoogleChrome/getGoogleChrome.sh
+++ getGoogleChrome/getGoogleChrome.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Define variables
-# Please follow guidlines outline by Google
+# Please follow guidelines outline by Google
 # https://support.google.com/chrome/a/answer/9915669?hl=en
 chrome_download_url="https://dl.google.com/chrome/mac/stable/accept_tos%3Dhttps%253A%252F%252Fwww.google.com%252Fintl%252Fen_ph%252Fchrome%252Fterms%252F%26_and_accept_tos%3Dhttps%253A%252F%252Fpolicies.google.com%252Fterms/googlechrome.pkg"
 temp_dmg_file="/Users/Shared/googlechrome.pkg"
